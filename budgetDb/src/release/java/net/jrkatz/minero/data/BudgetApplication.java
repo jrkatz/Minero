a32afb9c202685edb29a0dcb3993b786125b6c5e
Jacob Katz <jacob.r.katz@gmail.com>
2017-05-01 19:40:11 -0400

    stetho now only runs in debug variant

---
+++ budgetDb/src/release/java/net/jrkatz/minero/data/BudgetApplication.java
@@ -0,0 +1,33 @@
+/*
+ *     Minero is a minimal budget application
+ *     Copyright (C) 2017 Jacob Katz
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.jrkatz.minero.data;
+
+import android.app.Application;
+
+/**
+ * @Author jrkatz
+ * @Date 5/1/2017.
+ */
+
+public class BudgetApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
